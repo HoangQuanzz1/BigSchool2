0452c545f6bdc82845455281090f3f4eccb2f982
HoangQuanzz1 <tran.quan.qwe@gmail.com>
2022-03-28 11:51:19 +0700

    first commit

---
+++ lap3_TranHoangQuan_BigSchool/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using lap3_TranHoangQuan_BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using System.Data.Entity;
+
+namespace lap3_TranHoangQuan_BigSchool.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var upcommingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now && c.IsCanceled == false).ToList();
+            return View(upcommingCourses);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
